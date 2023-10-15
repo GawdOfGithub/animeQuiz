55cc2a8a4aa10d2da5c4c52a8a9aef6a4150d5be
Anurag Bhandari <anuragbhandari77736@gmail.com>
2023-10-15 06:45:47 +0530

    hello

---
+++ src/Contexts/MainContext.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import{}
+type Props = {hello}
+
+export default function MainContext({}: Props) {
+  return (
+    <div>MainContext</div>
+  )
+}rfrfrfrfrui
---
+++ src/assets/data.js
@@ -0,0 +1,16 @@
+const data = 
+[
+{
+    id:0,
+    question: 'Which of the following is the oldest anime',
+    points: 10,
+    options: {
+      a : Naurto,
+      b : OnePiece,
+      c : Pokemon,
+      d : DragonBall,
+    }
+    correct:d
+
+}
+]
