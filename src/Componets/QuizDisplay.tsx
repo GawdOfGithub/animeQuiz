945291ddd6363029873d51a4668547bf6cfed4c7
Anurag Bhandari <anuragbhandari77736@gmail.com>
2023-10-15 12:18:39 +0530

    Switched to exp1

--- src/Componets/QuizDisplay.tsx
+++ src/Componets/QuizDisplay.tsx
@@ -1,28 +1,41 @@
 import React from "react";
 import data from "../assets/data";
-
+import { useAnime } from "../Contexts/MainContext";
 type Props = {};
 
 export default function QuizDisplay({}: Props) {
+  const {number,setNumber} = useAnime()
   return (
     <div>
-    <div className="items-center align-center ml-20">
-      <img src="sasuke.svg" alt="anime image" style={{ width: "200px", height: "200px" }} />
-      </div>
-      <div className="">
-        {data.map((item, index) => (
-          <div key={index}>
-            <div className="text-2xl font-bold ml-3">{item.question}</div>
-            <div className="items-center flex flex-col mt-3">
-            <button className="text-2xl font-bold w-screen mb-5 rounded-3xl hover:bg-blue-600">{item.options.a}</button>
-            <button className="text-2xl font-bold w-screen mb-5 rounded-3xl  hover:bg-blue-600">{item.options.b}</button>
-            <button className="text-2xl font-bold w-screen mb-5 rounded-3xl  hover:bg-blue-600">{item.options.c}</button>
-            <button className="text-2xl font-bold w-screen mb-5 rounded-3xl  hover:bg-blue-600">{item.options.d}</button>
-            </div>
-          </div>
-        ))}
+      <div className="items-center align-center ml-20">
+        <img src="sasuke.svg" alt="anime image" style={{ width: "200px", height: "200px" }} />
       </div>
 
+      <div className="">
+        <div className="text-2xl font-bold ml-3">{data[number].question}</div>
+        <div className="items-center flex flex-col mt-3">
+          <button className="text-2xl font-bold w-screen mb-5 rounded-3xl hover:bg-blue-600" onClick={()=>
+          {
+            dispatch({
+              type:number,
+              payload:data[number].correct,
+
+            })
+            setNumber(number+1)}}>
+            {data[number].options.a}
+          </button>
+          <button className="text-2xl font-bold w-screen mb-5 rounded-3xl hover:bg-blue-600">
+            {data[number].options.b}
+          </button>
+          <button className="text-2xl font-bold w-screen mb-5 rounded-3xl hover:bg-blue-600">
+            {data[number].options.c}
+          </button>
+          <button className="text-2xl font-bold w-screen mb-5 rounded-3xl hover:bg-blue-600">
+            {data[number].options.d}
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
+efefefefefef
--- src/Contexts/MainContext.tsx
+++ src/Contexts/MainContext.tsx
@@ -2,19 +2,34 @@
 import React from 'react'
 import { useContext,createContext } from 'react'
 import { useState } from 'react'
+import { useReducer } from 'react'
+import data from '../assets/data'
 type Props = {}
 const MainContext = createContext(null)
-export default function Anime({children}: Props) {
-const[timer,setTimer] = useState(1)
+export default function Anime({children}) {
+  
+  const initialState = {}
+  const[number,setNumber] = useState(0)
+ const[state,dispatch] = useReducer(handleAnime,initialState)
+
+ function handleAnime(state,action)
+ {
+  switch(action.type){
+  case 0:
+    return {
+      
+    }
+  }
+ }
 {children}
   return (
-    <MainContext.Provider>
+    <MainContext.Provider value={{number,setNumber}}>
     {children}
     </MainContext.Provider>
   )
 }
 export function useAnime ()
 {
- useContext(MainContext)
+ return useContext(MainContext)
 }
 /* eslint-disable */
